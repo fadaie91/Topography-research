8a9c2928f4f86092da753bc3ac86ca4f233bca42
fadaie91 <85972119+fadaie91@users.noreply.github.com>
2021-08-27 11:05:07 -0400

    Add files via upload

---
+++ psiplot.jl
@@ -0,0 +1,75 @@
+ENV["GKSwstype"] = "nul"
+using Printf
+using Oceananigans
+using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBoundary
+using Oceananigans.BoundaryConditions: fill_halo_regions!
+
+using Plots
+
+using Oceananigans.ImmersedBoundaries: mask_immersed_field!
+
+function nice_divergent_levels(c, clim; nlevels=20)
+    levels = range(-clim, stop=clim, length=nlevels)
+    cmax = maximum(abs, c)
+    clim < cmax && (levels = vcat([-cmax], levels, [cmax]))
+    return (-clim, clim), levels
+end
+
+function nice_nondivergent_levels(c, clim; nlevels=20)
+    levels = range(0, stop=clim, length=nlevels)
+    cmax = maximum(abs, c)
+    clim < cmax && (levels = vcat([0], levels, [cmax]))
+    return (0, clim), levels
+end
+
+function nan_solid(y, z, v, seamount)
+    Ny, Nz = size(v)
+    y2 = reshape(y, Ny, 1)
+    z2 = reshape(z, 1, Nz)
+    v[seamount.(0, y2,z2)] .= NaN
+    return nothing
+end
+
+function show_mask(grid)
+
+    c = CenterField(CPU(), grid)
+    c .= 1
+
+    mask_immersed_field!(c)
+
+    x, y, z = nodes(c)
+
+    return x, y, z, c
+end
+
+grid = RegularRectilinearGrid(size=(16, 8), y=(-1, 1), z=(-1, 0),                           
+                              topology=(Flat, Periodic, Bounded))
+
+h0, L              = 0.5, 0.25
+seamount(x, y, z)  = z < - 1 + h0*exp(-y^2/L^2)  
+grid_with_seamount = ImmersedBoundaryGrid(grid, GridFittedBoundary(seamount))
+
+x, y, z, mask = show_mask(grid_with_seamount)
+plt = heatmap(y, z, interior(mask)[1,:,:]', xlabel = "y", ylabel = "z", title = "Masked Region")
+savefig(plt,"mask.png")
+
+Ψ = Field(Center, Face, Face, CPU(), grid_with_seamount)
+
+
+h(y)    = h0*exp(-y^2/L^2)
+ζ(y, z) = z/(h(y) - 1)
+set!(Ψ, (x, y, z) -> (1 - ζ(y, z))^2)
+fill_halo_regions!(Ψ, CPU())
+
+
+
+xp, yp, zp = nodes((Center, Face, Face),   grid)
+
+mask_immersed_field!(Ψ)
+
+
+Ψlims, Ψlevels = nice_nondivergent_levels(Ψ, 1)
+
+psiplot = contourf(yp, zp, interior(Ψ)[1,:,:]'; xlabel = "y", ylabel = "z", title = "Psi")
+savefig(psiplot, "Psi.png")
+
