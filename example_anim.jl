6cce8b6bdd03d38621fd98692a1feaf947cf50ce
fadaie91 <85972119+fadaie91@users.noreply.github.com>
2021-10-15 08:40:42 -0400

    Create example_anim.jl

---
+++ example_anim.jl
@@ -0,0 +1,25 @@
+ENV["GKSwstype"] = "nul"
+using Plots
+
+@userplot CirclePlot
+@recipe function f(cp::CirclePlot)
+    x, y, i = cp.args
+    n = length(x)
+    inds = circshift(1:n, 1 - i)
+    linewidth --> range(0, 10, length = n)
+    seriesalpha --> range(0, 1, length = n)
+    aspect_ratio --> 1
+    label --> false
+    x[inds], y[inds]
+end
+
+n = 150
+t = range(0, 2π, length = n)
+x = sin.(t)
+y = cos.(t)
+
+anim = @animate for i ∈ 1:n
+    circleplot(x, y, i)
+end
+mp4(anim, "anim_fps15.gif", fps = 15)
+#gif(anim, "anim_fps15.gif", fps = 15)
